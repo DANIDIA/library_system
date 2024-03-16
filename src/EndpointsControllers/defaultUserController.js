5e1c006ab203b7e9a1373956aa7684fcbf0e55ee
DANIDIA <danik.serduk@gmail.com>
2024-03-16 16:57:04 +0100

    add defaultController for users controllers

---
+++ src/EndpointsControllers/defaultUserController.js
@@ -0,0 +1,52 @@
+import sql from 'mysql-bricks';
+import { handleQuery } from '../Helpers/index.js';
+
+export class DefaultUserController {
+    constructor (tableName) {
+        this._tableName = tableName;
+    }
+
+    getOne () {
+        return async (req, res) => {
+            const id = req.body.id;
+
+            const query = sql
+                .select(`${this._tableName}.*, name, surname, phone_number, email, role, status, addition_time, login, password`)
+                .from('employee_account')
+                .join(this._tableName, { 'employee_account.id': 'employee_account_id' })
+                .where(sql.eq(`${this._tableName}.id`, id));
+
+            const { values, err } = await handleQuery(query);
+
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
+
+            res.status(200).json(values[0]);
+        };
+    }
+
+    getMany () {
+        return async (req, res) => {
+            const fromID = req.body.fromID;
+            const amount = req.body.amount;
+
+            const query = sql
+                .select(`${this._tableName}.*, name, surname, phone_number, email, role, status, addition_time, login, password`)
+                .from('employee_account')
+                .join(this._tableName, { 'employee_account.id': 'employee_account_id' })
+                .where(sql.gte(`${this._tableName}.id`, fromID))
+                .limit(amount);
+
+            const { values, err } = await handleQuery(query);
+
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
+
+            res.status(200).json(values);
+        };
+    }
+}
