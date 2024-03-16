4ab40bf51b149c7c3ab66be7660c7a5d1cb0f2b8
DANIDIA <danik.serduk@gmail.com>
2024-03-16 16:09:39 +0100

    improve code readability

--- src/Routers/departmentRouter.js
+++ src/Routers/departmentRouter.js
@@ -8,7 +8,7 @@ export const departmentRouter = express.Router();
 departmentRouter.use(authenticate);
 
 departmentRouter.post('/create', validateRoles([role.ADMIN]), departmentController.create());
-departmentRouter.get('/get_one', validateRoles(), async (req, res) => await departmentController.getOne(req, res));
-departmentRouter.get('/get_many', validateRoles(), async (req, res) => await departmentController.getMany(req, res));
+departmentRouter.get('/get_one', validateRoles(), departmentController.getOne());
+departmentRouter.get('/get_many', validateRoles(), departmentController.getMany());
 departmentRouter.get('/get_books', validateRoles(), departmentController.getBooks());
 departmentRouter.put('/change_data', validateRoles([role.ADMIN, role.DEPARTMENT_MANAGER]), departmentController.changeData());
