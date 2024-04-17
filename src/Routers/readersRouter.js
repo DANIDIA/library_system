97244bafaf3490ea200d32f69aea602978009822
DANIDIA <danik.serduk@gmail.com>
2024-04-17 15:05:20 +0200

    refactor(readersRouter): add returnBook handler to endpoint

--- src/Routers/readersRouter.js
+++ src/Routers/readersRouter.js
@@ -7,7 +7,7 @@ export const readersRouter = express.Router();
 readersRouter.use(authenticate);
 
 readersRouter.post('/add', validateRoles(), readersController.add());
-readersRouter.post('/return_book', validateRoles());
+readersRouter.post('/return_book', validateRoles(), readersController.returnBook());
 readersRouter.get('/get', validateRoles(), readersController.get());
 readersRouter.put('/update', validateRoles(), readersController.update());
 readersRouter.put('/changeStatus', validateRoles(), readersController.changeStatus());
