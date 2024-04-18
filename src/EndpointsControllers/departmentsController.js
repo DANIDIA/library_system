ba581cd264f47b1c119b7e408bb2b61e69ad236a
DANIDIA <danik.serduk@gmail.com>
2024-04-18 13:30:34 +0200

    refactor(departmentsController): rewrite and rename create handler

--- src/EndpointsControllers/departmentsController.js
+++ src/EndpointsControllers/departmentsController.js
@@ -1,5 +1,5 @@
 import { DefaultController } from './defaultController.js';
-import { handleQuery, recordExist } from '../Helpers/index.js';
+import { connection, handleQuery, recordExist } from '../Helpers/index.js';
 import sql from 'mysql-bricks';
 import { dbTablesNames } from '../enums/index.js';
 
@@ -8,27 +8,40 @@ export class DepartmentsController extends DefaultController {
         super(dbTablesNames.DEPARTMENTS);
     }
 
-    create () {
-        return async (req, res) => {
-            const managerID = req.body.actualManagerID;
+    add () {
+        return async (req, res, next) => {
+            try {
+                const managerID = req.body.actualManagerID;
 
-            if (!(await recordExist(managerID, 'department_manager'))) {
-                return res.status(404).send('Department manager not exist');
+                if (Object.hasOwn(req.body, 'actualManagerID') &&
+                    !(await recordExist(managerID, dbTablesNames.EMPLOYEES))) {
+                    return res.status(404).send(`Department manager with id ${managerID} doesn't exist`);
+                }
+
+                const fields = {
+                    name: true,
+                    address: true,
+                    contactNumber: true,
+                    actualManagerID: false
+                };
+
+                const values = this._getValuesFromRequestBody(req.body, fields);
+
+                if (typeof values === 'string') {
+                    return res.status(400).send(`Field with name '${values}' is necessary`);
+                }
+
+                const query = sql
+                    .insert(this._tableName, Object.keys(fields))
+                    .values(values)
+                    .toParams({ placeholder: '?' });
+
+                await connection.query(query.text, query.values);
+
+                res.status(200).send('ok');
+            } catch (e) {
+                next(e);
             }
-
-            const query = sql
-                .insert(this._tableName, 'name', 'address', 'contact_number', 'actual_manager')
-                .values(req.body.name, req.body.address, req.body.contactNumber, managerID)
-                .toParams({ placeholder: '?' });
-
-            const { values, err } = await handleQuery(query);
-
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            }
-
-            res.status(200).json(values[0].insertId);
         };
     }
 
