dfa00dfa54a09f4bf68a2f0629987c5109117068
DANIDIA <danik.serduk@gmail.com>
2024-04-18 12:50:21 +0200

    fix(usersRouter): adds handlers to endpoints

--- src/Routers/usersRouter.js
+++ src/Routers/usersRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authenticate, validateRoles } from '../Helpers/index.js';
+import { usersController } from '../EndpointsControllers/index.js';
 
 export const usersRouter = express.Router();
 
 usersRouter.use(authenticate);
 
-usersRouter.post('/add', validateRoles());
-usersRouter.post('/get', validateRoles());
-usersRouter.post('/update', validateRoles());
-usersRouter.post('/change_status', validateRoles());
+usersRouter.post('/add', validateRoles(), usersController.add());
+usersRouter.post('/get', validateRoles(), usersController.get());
+usersRouter.post('/update', validateRoles(), usersController.update());
+usersRouter.post('/change_status', validateRoles(), usersController.changeStatus());
