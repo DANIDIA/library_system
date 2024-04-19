037c60f2056b2e4f30cebe4c09816d3bbdcd9451
DANIDIA <danik.serduk@gmail.com>
2024-04-19 17:33:40 +0200

    refactor: removes using of validateRoles

--- src/Routers/booksRouter.js
+++ src/Routers/booksRouter.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { booksController } from '../EndpointsControllers/index.js';
-import { authenticate, validateRoles } from '../Helpers/index.js';
+import { authenticate } from '../Helpers/index.js';
 
 export const booksRouter = express.Router();
 
 booksRouter.use(authenticate);
 
-booksRouter.post('/add', validateRoles(), booksController.add());
-booksRouter.get('/get', validateRoles(), booksController.get());
-booksRouter.get('/given_amount', validateRoles(), booksController.givenAmount());
-booksRouter.post('/give_to_reader', validateRoles(), booksController.giveToReader());
-booksRouter.put('/update', validateRoles(), booksController.update());
-booksRouter.delete('/remove', validateRoles(), booksController.remove());
+booksRouter.post('/add', booksController.add());
+booksRouter.get('/get', booksController.get());
+booksRouter.get('/given_amount', booksController.givenAmount());
+booksRouter.post('/give_to_reader', booksController.giveToReader());
+booksRouter.put('/update', booksController.update());
+booksRouter.delete('/remove', booksController.remove());
--- src/Routers/readersRouter.js
+++ src/Routers/readersRouter.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { readersController } from '../EndpointsControllers/index.js';
-import { authenticate, validateRoles } from '../Helpers/index.js';
+import { authenticate } from '../Helpers/index.js';
 
 export const readersRouter = express.Router();
 
 readersRouter.use(authenticate);
 
-readersRouter.post('/add', validateRoles(), readersController.add());
-readersRouter.post('/return_book', validateRoles(), readersController.returnBook());
-readersRouter.get('/get', validateRoles(), readersController.get());
-readersRouter.put('/update', validateRoles(), readersController.update());
-readersRouter.put('/change_status', validateRoles(), readersController.changeStatus());
-readersRouter.delete('/remove', validateRoles(), readersController.remove());
+readersRouter.post('/add', readersController.add());
+readersRouter.post('/return_book', readersController.returnBook());
+readersRouter.get('/get', readersController.get());
+readersRouter.put('/update', readersController.update());
+readersRouter.put('/change_status', readersController.changeStatus());
+readersRouter.delete('/remove', readersController.remove());
