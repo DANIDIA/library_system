0e7d692be476fa634cfd96c93d6cd7b4f81d388f
DANIDIA <danik.serduk@gmail.com>
2024-04-29 11:05:27 +0200

    refactor: clear exports in indexes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,4 @@
 export * from './database.js';
 export * from './authenticate.js';
-export * from './validateRoles.js';
 export * from './helperDBFunctions.js';
 export * from './handleEndpointsErrors.js';
