3c19d9460b80a0fbacf572c2d2815445a3970743
DANIDIA <danik.serduk@gmail.com>
2024-04-29 10:12:31 +0200

    refactor: clear unused named import

--- src/accessRules/adminOnlyRule.js
+++ src/accessRules/adminOnlyRule.js
@@ -1,11 +1,11 @@
 import { getUserBySession } from '../helpers/index.js';
-import { role as roles } from '../enums/index.js';
+import { role } from '../enums/index.js';
 
 export function adminOnlyRule () {
     return async (req, res, next) => {
         const user = await getUserBySession(req.body.sessionID);
 
-        if (user.role !== roles.ADMIN) {
+        if (user.role !== role.ADMIN) {
             return res.status(400);
         }
 
--- src/accessRules/forManagerRule.js
+++ src/accessRules/forManagerRule.js
@@ -1,13 +1,13 @@
 import { getUserBySession } from '../helpers/index.js';
-import { role as roles } from '../enums/index.js';
+import { role } from '../enums/index.js';
 
 export function forManagerRule (departmentIDField) {
     return async (req, res, next) => {
         const user = await getUserBySession(req.body.sessionID);
 
-        if (user.role === roles.LIBRARIAN) {
+        if (user.role === role.LIBRARIAN) {
             return res.status(400);
-        } else if (user.role === roles.DEPARTMENT_MANAGER) {
+        } else if (user.role === role.DEPARTMENT_MANAGER) {
             if (req.body[departmentIDField] !== user.role.departmentID) {
                 return res.status(400);
             }
