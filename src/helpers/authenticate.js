41a678b3350decee42e227635aff5a1306aad39c
DANIDIA <danik.serduk@gmail.com>
2024-04-29 12:38:46 +0200

    refactor(authenticate): clear outdated check

--- src/helpers/authenticate.js
+++ src/helpers/authenticate.js
@@ -5,8 +5,7 @@ export async function authenticate (req, res, next) {
     const sessionID = req.body.sessionID;
     const status = await getSessionStatus(sessionID);
 
-    if (status === sessionStatus.IS_ENDED) { return res.status(400).send('Session ended'); }
-    if (status === sessionStatus.NOT_EXIST) { return res.status(400).send('Session not exist'); }
+    if (status === sessionStatus.NOT_EXIST) { return res.status(403).send('Session not exist'); }
 
     next();
 }
