bfb5b5c406c3d75dcb27c203d247fa2613d1c88b
DANIDIA <danik.serduk@gmail.com>
2024-06-07 20:06:10 +0200

    chore: add swagger packet and config it

---
+++ src/utils/swagger.js
@@ -0,0 +1,18 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Library system API',
+    },
+  },
+  apis: ['./src/routers/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export function swaggerDocs(app) {
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+}
