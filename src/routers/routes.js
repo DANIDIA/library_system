9144538ac6b5f612f02792089a3fcda964e5762f
DANIDIA <danik.serduk@gmail.com>
2024-06-08 21:28:03 +0200

    fix(routes): fix resources URLs

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -5,9 +5,9 @@ import { usersRouter } from './users.router.js';
 import { departmentsRouter } from './departments.router.js';
 
 export function routes(app) {
-  app.use('auth', authenticationRouter);
-  app.use('api/readers', readersRouter);
-  app.use('api/books', booksRouter);
-  app.use('api/users', usersRouter);
-  app.use('api/departments', departmentsRouter);
+  app.use('/auth', authenticationRouter);
+  app.use('/api/readers', readersRouter);
+  app.use('/api/books', booksRouter);
+  app.use('/api/users', usersRouter);
+  app.use('/api/departments', departmentsRouter);
 }
