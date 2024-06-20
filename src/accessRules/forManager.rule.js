20e223c9a968388c61b7ece77214adf52c860543
DANIDIA <danik.serduk@gmail.com>
2024-06-20 17:46:56 +0200

    chore(forManager.rule): rework

--- src/accessRules/forManager.rule.js
+++ src/accessRules/forManager.rule.js
@@ -1,18 +1,25 @@
 import { getUserBySession } from '../helpers/index.js';
 import { rolesEnum } from '../shared/index.js';
 
-export function forManagerRule(departmentIDField) {
+export function forManagerRule(departmentIdField, inRequestBody = true) {
   return async (req, res, next) => {
-    const user = await getUserBySession(
-      req.body.sessionID || req.query.sessionID
-    );
+    const user = await getUserBySession(req.cookies.sessionID);
 
     if (user.role === rolesEnum.LIBRARIAN) {
-      return res.status(400);
-    } else if (user.role === rolesEnum.DEPARTMENT_MANAGER) {
-      if (req.body[departmentIDField] !== user.role.departmentID) {
-        return res.status(400);
-      }
+      return res.status(403).send();
+    }
+
+    const valueToCheck = inRequestBody
+      ? req.body[departmentIdField]
+      : req.params[departmentIdField];
+
+    if (
+      user.role === rolesEnum.DEPARTMENT_MANAGER &&
+      valueToCheck !== user.departmentID
+    ) {
+      res.statusMessage =
+        'You do not have permission as manager of another department';
+      return res.status(403);
     }
 
     next();
