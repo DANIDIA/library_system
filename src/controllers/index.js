919d6617e6fb30d92dfd5237255f5799a7baedf8
DANIDIA <danik.serduk@gmail.com>
2024-06-20 12:07:44 +0200

    refactor: rename department.controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 export * from './authentication.controller.js';
 export * from './readers.controller.js';
 export * from './books.controller.js';
-export * from './department.controller.js';
+export * from './departments.controller.js';
 export * from './users.controller.js';
