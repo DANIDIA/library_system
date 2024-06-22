29563a52cdb30a69dee7a78daef2ed939306365e
DANIDIA <danik.serduk@gmail.com>
2024-06-22 12:37:30 +0200

    chore(users.router): use schemas for endpoints

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import { usersController } from '../controllers/index.js';
 import { forManagerRule } from '../accessRules/index.js';
-import { authenticate } from '../middleware/index.js';
+import { authenticate, validateScheme } from '../middleware/index.js';
+import {
+  defaultUsersScheme,
+  queryUsersScheme,
+  updateUserScheme,
+} from '../schemas/users.shemas.js';
 
 export const usersRouter = express.Router();
 
 usersRouter.use(authenticate);
 usersRouter.use(forManagerRule('departmentID'));
 
-usersRouter.post('/add', usersController.add());
-usersRouter.get('/get', usersController.get());
-usersRouter.put('/update', usersController.update());
+usersRouter.post(
+  '/add',
+  validateScheme(defaultUsersScheme),
+  usersController.add()
+);
+usersRouter.get(
+  '/get',
+  validateScheme(queryUsersScheme),
+  usersController.get()
+);
+usersRouter.put(
+  '/update',
+  validateScheme(updateUserScheme),
+  usersController.update()
+);
 usersRouter.put('/change_status', usersController.changeStatus());
