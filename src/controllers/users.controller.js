bfe492bcf823d81f03184531db3093cd727f52b3
DANIDIA <danik.serduk@gmail.com>
2024-06-24 14:44:25 +0200

    feat(users.controller): rework update-controller, improve role check, add changing of amount of employees when department id is changed, add changing of actual manager of department when changes role of librarian or managers department id changes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,23 +1,25 @@
 import passwordGenerator from 'generate-password';
-import { DefaultController } from './default.controller.js';
-import { dbTablesNamesEnum, rolesEnum } from '../shared/index.js';
+import {
+  accountStatusesEnum,
+  dbTablesNamesEnum,
+  rolesEnum,
+} from '../shared/index.js';
 import {
   changeRecordData,
   connection,
   createRecord,
+  endAllUserSessions,
   getDepartmentByID,
   getUserBySession,
   increaseEmployeeAmountByOne,
-  recordExist,
+  increaseValueBy,
+  queryRecords,
 } from '../helpers/index.js';
-import {
-  validateEmail,
-  validatePhoneNumber,
-} from '../helpers/contactDetailsValidators.js';
 import { getSchemeFields } from './helpers.js';
 import {
   defaultUsersScheme,
   queryUsersScheme,
+  updateUserScheme,
 } from '../schemas/users.shemas.js';
 import sql from 'mysql-bricks';
 import { usersResourceFieldsNames } from './shared/index.js';
@@ -111,58 +113,65 @@ export async function queryUsersController(req, res, next) {
   }
 }
 
-class UsersController extends DefaultController {
-  constructor() {
-    const updatableFields = [
-      'name',
-      'surname',
-      'phoneNumber',
-      'email',
-      'login',
-      'password',
-      'departmentID',
-    ];
-    super(dbTablesNamesEnum.EMPLOYEES, updatableFields);
-  }
+export async function updateUserController(req, res, next) {
+  try {
+    const scheme = getSchemeFields(req, updateUserScheme);
+    const author = await getUserBySession(req.cookies.sessionID);
+    const userID = req.params.id;
+    const user = await getUserByID(userID);
+    const userDepartmentID = user.departmentID;
+    const putDepartmentID = scheme.body.departmentID;
+    const putDepartment = await getDepartmentByID(putDepartmentID);
 
-  update() {
-    return async (req, res, next) => {
-      try {
-        if (
-          Object.hasOwn(req.body, 'departmentID') &&
-          !(await recordExist(
-            req.body.departmentID,
-            dbTablesNamesEnum.DEPARTMENTS
-          ))
-        ) {
-          return res
-            .status(404)
-            .send(`Department with id ${req.body.departmentID} doesn't exist`);
-        }
+    const isChangingRole = scheme.body.role !== user.role;
+    const isChangingDepartment = user.departmentID !== scheme.body.departmetnID;
+    const isPuttingManager = scheme.body.role === rolesEnum.DEPARTMENT_MANAGER;
 
-        if (validateEmail(req, res) || validatePhoneNumber(req, res)) {
-          return;
-        }
+    if (
+      rolePermissionLevel[author.role] <= rolePermissionLevel[user.role] ||
+      rolePermissionLevel[author.role] <= rolePermissionLevel[scheme.body.role]
+    ) {
+      res.statusMessage =
+        "You don't have permission to manipulate with users with your permission level or higher";
+      return res.status(403).send();
+    }
 
-        await super.update(req, res);
-      } catch (e) {
-        next(e);
-      }
-    };
-  }
+    if (
+      (isChangingRole || isChangingDepartment) &&
+      isPuttingManager &&
+      putDepartment?.actualManagerID !== null
+    ) {
+      res.statusMessage = `Department with id '${putDepartmentID}' has already department manager`;
+      return res.status(406).send();
+    }
 
-  changeStatus() {
-    return async (req, res, next) => {
-      try {
-        await super.changeStatus(req, res);
-      } catch (e) {
-        next(e);
-      }
-    };
+    await updateUser(userID, scheme.body);
+
+    if (scheme.body.status === accountStatusesEnum.BLOCKED) {
+      await endAllUserSessions(userID);
+    }
+
+    if (
+      (isChangingRole && scheme.body.role === rolesEnum.LIBRARIAN) ||
+      (isChangingDepartment && isPuttingManager && !isChangingRole)
+    ) {
+      await setManagerInDepartment(userDepartmentID, sql('null'));
+    }
+
+    if (isPuttingManager && (isChangingRole || isChangingDepartment)) {
+      await setManagerInDepartment(putDepartmentID, userID);
+    }
+
+    if (user.departmentID !== scheme.body.departmentID) {
+      await decreaseEmployeesAmountByOne(userDepartmentID);
+      await increaseEmployeeAmountByOne(putDepartmentID);
+    }
+
+    res.status(200).send();
+  } catch (e) {
+    next(e);
   }
 }
-
-export const usersController = new UsersController();
 
 async function setActualDepartmentManager(actualManagerID, departmentID) {
   await changeRecordData(departmentID, dbTablesNamesEnum.DEPARTMENTS, {
@@ -173,10 +182,7 @@ async function setActualDepartmentManager(actualManagerID, departmentID) {
 async function checkAuthorRolePermission(authorID, roleTryingToSet, res) {
   const author = await getUserBySession(authorID);
 
-  if (
-    author.role === rolesEnum.DEPARTMENT_MANAGER &&
-    roleTryingToSet !== rolesEnum.LIBRARIAN
-  ) {
+  if (rolePermissionLevel[author.role] < rolePermissionLevel[roleTryingToSet]) {
     res.statusMessage =
       "You don't have permission to manipulate with users with permission level manager or higher";
     res.status(403).send();
@@ -215,4 +221,33 @@ async function queryUsers(valuesToQuery) {
     .toParams({ placeholder: '?' });
 
   return (await connection.qeury(query.text, query.values))[0];
+}
+
+async function getUserByID(id) {
+  return (await queryRecords(dbTablesNamesEnum.EMPLOYEES, { id }))[0];
+}
+
+const rolePermissionLevel = Object.freeze({
+  [rolesEnum.ADMIN]: 3,
+  [rolesEnum.DEPARTMENT_MANAGER]: 2,
+  [rolesEnum.LIBRARIAN]: 1,
+});
+
+async function decreaseEmployeesAmountByOne(departmentID) {
+  await increaseValueBy(
+    dbTablesNamesEnum.DEPARTMENTS,
+    departmentID,
+    'employeesAmount',
+    -1
+  );
+}
+
+async function updateUser(id, data) {
+  await changeRecordData(id, dbTablesNamesEnum.EMPLOYEES, data);
+}
+
+async function setManagerInDepartment(id, managerID) {
+  await changeRecordData(id, dbTablesNamesEnum.DEPARTMENTS, {
+    actualManagerID: managerID,
+  });
 }
