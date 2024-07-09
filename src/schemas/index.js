b83e9e22f4284113f467d906e9047acf7fe537b4
DANIDIA <danik.serduk@gmail.com>
2024-07-09 15:40:17 +0200

    feat: add schemas for readers

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,3 +2,4 @@ export * from './authentication.schemas.js';
 export * from './departments.schemas.js';
 export * from './pagination.schemas.js';
 export * from './book.schemas.js';
+export * from './readers.schemas.js';
---
+++ src/schemas/readers.schemas.js
@@ -0,0 +1,53 @@
+import { defineSchemeField } from './helpers/defineSchemeField.js';
+import { schemeFieldTypesEnum } from './shared/schemeFieldTypesEnum.js';
+import { phoneNumberValidator, emailValidator } from '../validators/index';
+import { dbTablesNamesEnum } from '../shared/index.js';
+
+export const defaultReadersScheme = Object.freeze({
+  body: {
+    name: defineSchemeField(schemeFieldTypesEnum.STRING).setMaxLength(30),
+    surname: defineSchemeField(schemeFieldTypesEnum.STRING).setMaxLength(40),
+    phoneNumber: defineSchemeField(schemeFieldTypesEnum.STRING)
+      .setMinValue(9)
+      .setMaxLength(12)
+      .setValidator(phoneNumberValidator),
+  },
+  email: defineSchemeField(schemeFieldTypesEnum.STRING)
+    .setMaxLength(45)
+    .setValidator(emailValidator),
+  status: defineSchemeField(schemeFieldTypesEnum.BOOL),
+});
+
+export const queryReadersScheme = Object.freeze({
+  query: {
+    name: defineSchemeField(schemeFieldTypesEnum.STRING).setRequired(false),
+    surname: defineSchemeField(schemeFieldTypesEnum.STRING).setRequired(false),
+    phoneNumber: defineSchemeField(schemeFieldTypesEnum.STRING).setRequired(
+      false
+    ),
+    email: defineSchemeField(schemeFieldTypesEnum.STRING).setRequired(false),
+    status: defineSchemeField(schemeFieldTypesEnum.STRING).setRequired(false),
+  },
+});
+
+export const accessReaderScheme = Object.freeze({
+  params: {
+    id: defineSchemeField(schemeFieldTypesEnum.STRING).setCheckAsID(
+      dbTablesNamesEnum.READERS
+    ),
+  },
+});
+
+export const returnBookScheme = Object.freeze({
+  params: {
+    readerID: accessReaderScheme.params.id,
+    bookID: defineSchemeField(schemeFieldTypesEnum.STRING).setCheckAsID(
+      dbTablesNamesEnum.BOOKS
+    ),
+  },
+});
+
+export const updateReaderScheme = Object.freeze({
+  ...accessReaderScheme,
+  ...defaultReadersScheme,
+});
