c7f08d77191fac42b198bc5f3f7aace940555dc4
DANIDIA <danik.serduk@gmail.com>
2024-07-12 23:23:10 +0200

    fix: fix books-query - now endpoint can return in response books that hasn't authors

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -276,19 +276,16 @@ async function getBookAmountInDepartment(bookID, departmentID) {
 
 async function queryBooksIDs({ title, authorsIDs = [] }) {
   let query = sql
-    .select('bookID')
-    .from(dbTablesNamesEnum.BOOK_AUTHORS)
-    .join(dbTablesNamesEnum.BOOKS)
-    .on(
-      `${dbTablesNamesEnum.BOOKS}.id`,
-      `${dbTablesNamesEnum.BOOK_AUTHORS}.bookID`
-    );
+    .select(`${dbTablesNamesEnum.BOOKS}.id`)
+    .from([dbTablesNamesEnum.BOOKS, dbTablesNamesEnum.BOOK_AUTHORS]);
 
   const conditions = [];
 
   title && conditions.push(sql.eq('title', title));
-  authorsIDs.length > 0 &&
+  if (authorsIDs.length > 0) {
+    conditions.push(sql.eq(`${dbTablesNamesEnum.BOOKS}.id`, sql('bookID')));
     conditions.push(sql.or(authorsIDs.map((id) => sql.eq('authorID', id))));
+  }
 
   if (conditions.length > 0) {
     query = query.where(sql.and(conditions));
@@ -296,10 +293,10 @@ async function queryBooksIDs({ title, authorsIDs = [] }) {
 
   query = query.toParams({ placeholder: '?' });
 
-  query.text += ` GROUP BY bookID HAVING COUNT(*) >= ${authorsIDs.length}`;
+  query.text += ` GROUP BY ${dbTablesNamesEnum.BOOKS}.id HAVING COUNT(*) >= ${authorsIDs.length}`;
 
   return (await connection.query(query.text, query.values))[0].map(
-    (record) => record.bookID
+    (record) => record.id
   );
 }
 
